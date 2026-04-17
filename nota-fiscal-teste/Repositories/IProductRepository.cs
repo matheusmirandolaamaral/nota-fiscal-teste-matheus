d16dcb3ee6b1e25f6df3fda51b862126ad4f4030
Matheus Mirandola do Amaral <matheusmirandola.amaral@gmail.com>
2026-04-17 16:46:24 -0300

    feat: implement repository pattern for Product entity

---
+++ nota-fiscal-teste/Repositories/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using nota_fiscal_teste.Model;
+
+namespace nota_fiscal_teste.Repositories
+{
+    public interface IProductRepository
+    {
+        List<Product> FindAll();
+        Product FindById(long id);
+        Product Create(Product product);
+        Product Update(Product product);
+        void Delete(long id);
+    }
+}
---
+++ nota-fiscal-teste/Repositories/Impl/ProductRepositoryImpl.cs
@@ -0,0 +1,50 @@
+﻿using nota_fiscal_teste.Model;
+using nota_fiscal_teste.Model.Context;
+
+namespace nota_fiscal_teste.Repositories.Impl
+{
+    public class ProductRepositoryImpl : IProductRepository
+    {
+        private MSSQLContext _context;
+
+        public ProductRepositoryImpl(MSSQLContext context)
+        {
+            _context = context;
+        }
+        public List<Product> FindAll()
+        {
+            return _context.Products.ToList();
+        }
+
+        public Product FindById(long id)
+        {
+            return _context.Products.Find(id);
+        }
+
+        public Product Create(Product product)
+        {
+            _context.Add(product);
+            _context.SaveChanges();
+            return product;
+        }
+
+        public Product Update(Product product)
+        {
+            var existingProduct = _context.Products.Find(product.Id);
+            if (existingProduct == null) return null;
+
+            _context.Entry(existingProduct).CurrentValues.SetValues(product);
+            _context.SaveChanges();
+            return product;
+        }
+        public void Delete(long id)
+        {
+            var existingProduct = _context.Products.Find(id);
+            if(existingProduct == null) return;
+
+            _context.Remove(existingProduct);
+            _context.SaveChanges();
+
+        }
+    }
+}
