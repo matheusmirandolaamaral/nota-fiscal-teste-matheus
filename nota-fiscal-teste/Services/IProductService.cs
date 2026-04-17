7de16870896a0c21a62bc1e35be10cd1459461b9
Matheus Mirandola do Amaral <matheusmirandola.amaral@gmail.com>
2026-04-17 16:48:03 -0300

    feat: implement product service

---
+++ nota-fiscal-teste/Services/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using nota_fiscal_teste.Model;
+
+namespace nota_fiscal_teste.Services
+{
+    public interface IProductService
+    {
+        List<Product> FindAll();
+        Product FindById(long id);
+        Product Create(Product product);
+        Product Update(Product product);
+        void Delete(long id);
+    }
+}
---
+++ nota-fiscal-teste/Services/Impl/ProductServiceImpl.cs
@@ -0,0 +1,37 @@
+﻿using nota_fiscal_teste.Model;
+using nota_fiscal_teste.Repositories;
+
+namespace nota_fiscal_teste.Services.Impl
+{
+    public class ProductServiceImpl : IProductService
+    {
+
+        private IProductRepository _repository;
+        public ProductServiceImpl(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+        public List<Product> FindAll()
+        {
+            return _repository.FindAll();
+        }
+
+        public Product FindById(long id)
+        {
+            return _repository.FindById(id);
+        }
+        public Product Create(Product product)
+        {
+            return _repository.Create(product);
+        }
+
+        public Product Update(Product product)
+        {
+            return _repository.Update(product);
+        }
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        }
+    }
+}
